cd1e1761dfae06f8a3e4d6adf1734152ae7411be
satyasaiarunkumar3-cmyk <satyasaiarunkumar3@gmail.com>
2026-01-20 00:50:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -143,4 +143,12 @@ def buy_giftcard():
     return redirect(url_for("thankyou"))
 
 # ================= RUN APPLICATION =================
-if __name__ == "__main
+if __name__ == "__main__":
+    init_db()
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 10000)),
+        debug=False
+    )
+
+
